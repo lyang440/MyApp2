04ce01bb98d42292a1fb4909559b247ac005cfa6
wangkechun <hi@hi-hi.cn>
2016-02-07 21:55:34 +0800

    support css in jade

--- src/components/App/Main.js
+++ src/components/App/Main.js
@@ -3,13 +3,14 @@ import styles from './Main.scss';
 import Content from './Content.jade';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
+const p = console.log.bind(console);
 
 const Main = React.createClass({
   render() {
-    return <div className={styles.root} >
-      <Content/>
-          </div>;
+    return <div className={styles.root}> {Content()} </div>;
   },
 });
 
 export default withStyles(Main, styles);
+
+
