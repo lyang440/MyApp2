4f16593915529b14a91d0ecdd7ce596bc763a0c0
wangkechun <hi@hi-hi.cn>
2016-02-07 10:29:27 +0800

    add scss 支持

--- src/client.js
+++ src/client.js
@@ -1,6 +1,46 @@
 import DOM from './components/App/';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WithContext from "react-with-context"
 
-ReactDOM.render(<DOM/>, document.getElementById('app'));
+
+let cssContainer = document.getElementById('css');
+const appContainer = document.getElementById('app');
+
+const context = {
+  insertCss: styles => styles._insertCss(),
+  onSetTitle: value => document.title = value,
+  onSetMeta: (name, content) => {
+    // Remove and create a new <meta /> tag in order to make it work
+    // with bookmarks in Safari
+    const elements = document.getElementsByTagName('meta');
+    [].slice.call(elements).forEach((element) => {
+      if (element.getAttribute('name') === name) {
+        element.parentNode.removeChild(element);
+      }
+    });
+    const meta = document.createElement('meta');
+    meta.setAttribute('name', name);
+    meta.setAttribute('content', content);
+    document.getElementsByTagName('head')[0].appendChild(meta);
+  },
+};
+
+let Con = React.createClass({
+  contextTypes: {
+      insertCss: React.PropTypes.func.isRequired,
+      onSetTitle: React.PropTypes.func.isRequired,
+      onSetMeta: React.PropTypes.func.isRequired,
+  },
+  render(){
+    return <WithContext context={context}>
+          <DOM/>
+        </WithContext>;
+  }
+})
+
+window.React = React;
+window.ReactDOM = ReactDOM;
+ReactDOM.render(<Con/>, appContainer);
+
+
--- src/components/App/Main.js
+++ src/components/App/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from './Main.scss';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 const Main = React.createClass({
   render() {
-    return <h1>hello world</h1>;
+    return <h1 className={styles.h} >hello world</h1>;
   },
 });
 
-export default Main;
+export default withStyles(Main, styles);
